def7a02d5762c128cc7ead0e6be88bdf79df1ad6
Claudia Boden <clauboden@hotmail.com>
2022-09-13 22:25:20 +0100

    Styled performance fields

--- models/Performance.js
+++ models/Performance.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const performanceSchema = mongoose.Schema({
 nameActivity: String,
 timeActivity: String,
+distanceActivity: String,
 locationActivity: String,
 dateActivity: Date
 })
